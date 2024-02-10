e8406cb152a8f3e0b0395a6e769c93f4f038a2d4
El Ech Iheb <ihebelleuch6@gmail.com>
2024-02-10 02:43:37 +0100

    First Commit
    2024-02-10

---
+++ resources/views/components/frontend/about.blade.php
@@ -0,0 +1,31 @@
+<section id="about" class="section bg-light-secondary dark:bg-dark-secondary">
+    <div class="container mx-auto">
+        <div class="flex flex-col xl:flex-row gap-24">
+            <img class="object-cover h-full w-[566px] md:mx-auto lg:mx-0 rounded-2xl" src="/img/about.jpg"
+                alt="About img" />
+            <div class="flex flex-col items-center text-center lg:items-start lg:text-left">
+                <div class="flex flex-col">
+                    <h2 class="text-3xl lg:text-4xl font-medium lg:font-extrabold mb-3">
+                        About Me
+                    </h2>
+                    <p class="mb-4 text-accent">Etudiant & Developpeur.</p>
+                    <hr class="mb-8 opacity-90 dark:opacity-5" />
+                    <p class="mb-8">
+                        Je suis actuellement étudiant en 3ème année licence en informatiques spécialisé génie
+                        logiciel et systèmes d'information.
+                        Rigoureux et capable de synthétiser l'information facilement. Motivé et proactif, je cherche
+                        une opportunité qui me permettra de renforcer mes compétences en développement
+                        web/mobile , IOT , AI et de découvrir les défis des entreprises.
+                        <br />
+                        <br />
+                        
+                    </p>
+                </div>
+                <a href="https://www.facebook.com/profile.php?id=100066967402885"
+                    class="btn btn-md bg-accent text-light-tail-100">
+                    Work with me
+                </a>
+            </div>
+        </div>
+    </div>
+</section>
